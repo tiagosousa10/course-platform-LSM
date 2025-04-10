6073ee655ea83ed6be8dcdcd7892c9a98d8abf8c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-10 11:44:36 +0100

    first steps on layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +23,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -32,6 +30,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-    </ClerkProvider>
   );
 }
