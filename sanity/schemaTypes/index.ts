f429ba2b42e76d95a30e595877c5f9682c1ef697
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-12 11:11:59 +0100

    changing the sanity structure to organize/view more effectively

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,24 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import {studentType} from "./studentType"
+import { blockContent } from './blockContent'
+import { categoryType } from './categoryType'
+import { courseType } from './courseType'
+import { enrollmentType } from './enrollmentType'
+import { instructorType } from './instructorType'
+import { lessonCompletionType } from './lessonCompletionType'
+import { lessonType } from './lessonType'
+import { moduleType } from './moduleType'
+
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [studentType],
-}
+  types: [
+    studentType,
+    blockContent,
+    categoryType,
+    courseType,
+    enrollmentType,
+    instructorType,
+    lessonCompletionType,
+    lessonType,
+    moduleType],
+    }
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -3,5 +3,40 @@ import type {StructureResolver} from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title('Admin Dashboard')
+    .items([
+      // Course Content
+      S.listItem()
+        .title("Course Content")
+        .child(
+          S.documentTypeList("course")
+            .title("Courses")
+            .child((courseId) =>
+              S.list()
+                .title("Course Options")
+                .items([
+                  // Option to edit course content
+                  S.listItem()
+                    .title("Edit Course Content")
+                    .child(
+                      S.document().schemaType("course").documentId(courseId)
+                    ),
+                  // Option to view course enrollments
+                  S.listItem()
+                    .title("View Students")
+                    .child(
+                      S.documentList()
+                        .title("Course Enrollments")
+                        .filter(
+                          '_type == "enrollment" && course._ref == $courseId'
+                        )
+                        .params({ courseId })
+                    ),
+                ])
+            )
+        ),
+
+        S.divider(),
+
+        
+    ])
