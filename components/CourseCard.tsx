a59d552421b6018a64eb538af893f5752f6f77de
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-13 19:29:51 +0100

    loader created

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,4 +1,6 @@
 import { GetCoursesQueryResult } from "@/sanity.types";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
 import Link from "next/link";
 
 interface CourseCardProps {
@@ -15,7 +17,22 @@ const CourseCard = ({course, progress, href}: CourseCardProps) => {
       className="group hover:no-underline flex"
    >
       <div className="bg-card rounded-xl overflow-hidden shadow-lg transition-all duration-300 ease-in-out hover:shadow-xl hover:translate-y-[-4px] border border-border flex flex-col flex-1">
-         Test
+         <div className="relative h-52 w-full overflow-hidden">
+            {course.image ? (
+               <Image
+                  src={urlFor(course.image).url() || ""}
+                  alt={course.title || "Course Image"}
+                  fill
+                  className="object-cover transition-transform duration-300 group-hover:scale-110"
+               
+               />
+            ) : (
+               <div className="h-full w-full flex items-center justify-center bg-muted">
+                  <Loader  size={"lg"}/>
+
+               </div>
+            )}
+         </div>
       </div>
    </Link>
   )
