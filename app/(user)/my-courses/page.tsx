bc397960b051105c40aa754a3747afc5db063a81
tiagosousa <tiagosousa.tams@hotmail.com>
2025-04-16 14:44:56 +0100

    getCourseProgress query with sanity

--- app/(user)/my-courses/page.tsx
+++ app/(user)/my-courses/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import { getEnrolledCourses } from '@/sanity/lib/courses/getEnrolledCourses'
+import { getCourseProgress } from '@/sanity/lib/courses/getCourseProgress'
 
 const MyCoursesPage = async() => {
    const user = await currentUser()
@@ -17,8 +18,10 @@ const MyCoursesPage = async() => {
    const coursesWithProgress = await Promise.all(
       enrolledCourses.map(async ({course}) => {
          if(!course) return null;
+         const progress = await getCourseProgress(user.id, course._id);
          return {
-
+            course, 
+            progress: progress.courseProgress,
          }
       })
    )
